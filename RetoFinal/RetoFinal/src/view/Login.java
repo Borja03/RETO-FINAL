f91957d0017fc6e9bcce23f5b04dadeb616c7b26
8xBit <multitartanga@gmail.com>
2024-04-09 08:30:14 +0200

    premier dia

--- RetoFinal/RetoFinal/src/view/Login.java
+++ RetoFinal/RetoFinal/src/view/Login.java
@@ -5,12 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private JComboBox comboBox ;
 	/**
 	 * Launch the application.
 	 */
@@ -32,11 +41,44 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 641, 556);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Usuario");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(142, 182, 114, 25);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblContrasea = new JLabel("Contraseña");
+		lblContrasea.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblContrasea.setBounds(142, 246, 114, 25);
+		contentPane.add(lblContrasea);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnNewButton.setBounds(281, 437, 106, 46);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(272, 243, 201, 35);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(272, 182, 201, 40);
+		contentPane.add(passwordField);
+		
+		comboBox = new JComboBox();
+		comboBox.setFont(new Font("Tahoma", Font.BOLD, 14));
+		comboBox.addItem("Admin");
+		comboBox.addItem("Entrenador");
+		comboBox.addItem("Jugador");
 
+		comboBox.setBounds(272, 350, 191, 35);
+		contentPane.add(comboBox);
+	}
 }
---
+++ RetoFinal/RetoFinal/src/view/MenuAdmin.java
@@ -0,0 +1,50 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class MenuAdmin extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuAdmin frame = new MenuAdmin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuAdmin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 562, 431);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Hi Admin");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(194, 155, 158, 33);
+		contentPane.add(lblNewLabel);
+	}
+
+}
