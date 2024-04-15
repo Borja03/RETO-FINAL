c7bccbbe7673519bc801e1adc60067e3adc1fc2c
Omar <144680229+8xbit@users.noreply.github.com>
2024-04-15 09:37:17 +0200

    Update BasesDeDatosRetoFinal.sql

--- RetoFinal/src/dataBase/BasesDeDatosRetoFinal.sql
+++ RetoFinal/src/dataBase/BasesDeDatosRetoFinal.sql
@@ -43,3 +43,7 @@ nombreEquipo varchar(30),
 foreign key (nombreEquipo) references equipo(nombreEquipo) on delete cascade
 );
 
+
+insert into equipo values('Athletic de Bilbao',100,'San Mames');
+insert into entrenador values('entrenador','1234','1','PrimerEntrenador','Athletic de Bilbao');
+insert into jugador values('jugador','1234','1',9,99,500,'Athletic de Bilbao');
