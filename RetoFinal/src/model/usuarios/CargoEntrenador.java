ec8bca6d53156d174d2e500361f34ca97c088e40
Alder <aldersanchez163@gmail.com>
2024-04-17 13:53:15 +0200

    merge hecho

--- RetoFinal/src/model/usuarios/CargoEntrenador.java
+++ RetoFinal/src/model/usuarios/CargoEntrenador.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model.usuarios;
 
 public enum CargoEntrenador {
@@ -14,20 +13,3 @@ public enum CargoEntrenador {
 		return nombre;
 	}
 }
-=======
-package model.usuarios;
-
-public enum CargoEntrenador {
-	PRIMER_ENTRENADOR("Primer entrenador"), SEGUNDO_ENTRENADOR("Segundo entrenador");
-
-	private final String nombre;
-
-	CargoEntrenador(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-}
->>>>>>> 64ece0c9f44ad5f2b9282575d684f969240645f3
